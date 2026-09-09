cd0932a8528ddeb5e438359046a4f2534bcbc494
tteletubbie <ttb.snow2@gmail.com>
2026-09-08 22:55:15 -0600

    Final Bank Account problem solved

--- unit1/bankAccount.py
+++ unit1/bankAccount.py
@@ -5,20 +5,28 @@ class BankAccount:
 
     def deposit(self, money):
         self.__balance += money
+        print(f"Deposited ${money} to {self.holder}'s account!")
 
     def withdraw(self, money):
+        if money <= 0:
+            print(f"It's impossible to withdraw ${money}")
+
         if money > self.__balance:
             print("You don't have the money to that request")
         else:
             self.__balance -= money
+            print(f"Withdrew ${money} from {self.holder}'s account!")
 
     def checkStatus(self):
-        print("-------------")
+        print("\n-------------")
         print(f"The current balance is ${self.__balance}")
+        print("-------------\n")
 
 
-galileoBank = BankAccount("Galileo", 0)
-galileoBank.deposit(100)
-galileoBank.checkStatus()
-galileoBank.withdraw(20)
-galileoBank.checkStatus()
+print("\033c")
+leslyBank = BankAccount("Lesly", 0)
+print(f"{leslyBank.holder} has ${leslyBank._BankAccount__balance}\n")
+leslyBank.deposit(100)
+leslyBank.checkStatus()
+leslyBank.withdraw(50)
+leslyBank.checkStatus()
