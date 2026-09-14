0e5417f18e22cc30ad43833fcf062eea26536935
tteletubbie <ttb.snow2@gmail.com>
2026-09-14 09:23:16 -0600

    Add Lab2

---
+++ unit1/Lab2_professor.py
@@ -0,0 +1,28 @@
+class Student:
+    def __init__(self, name):
+        self.name = name
+
+    def enroll(self, course):
+        self.course = course
+
+    def show_course(self):
+        print(f"{self.name} is enrolled in {self.course.name}")
+
+
+class Course:
+    def __init__(self, name):
+        self.name = name
+
+    def show(self):
+        print(f"Course: {self.name}")
+
+
+# Instances
+student = Student("Carlos")
+course = Course("Python Programming")
+
+# Creating a relationship
+student.enroll(course)
+
+# Use the Relationship
+student.show_course()
---
+++ unit1/Lab2_relation.py
@@ -0,0 +1,66 @@
+class User:
+    def __init__(self, name, surname, username, password):
+        self.name = name
+        self.surname = surname
+        self.username = username
+        self._password = password
+        self.posts = []
+
+    def add_post(self, post):
+        self.posts.append(post)
+        print(f"The user {self.username} created a post.")
+
+    def show_posts(self):
+        print(f"\nPosts by {self.username}:")
+        for post in self.posts:
+            post.print_post()
+
+
+class Post:
+    def __init__(self, description, user, likes=0):
+        self.description = description
+        self.likes = likes
+        self.user = user
+
+    def increment_likes(self):
+        self.likes += 1
+
+    def print_post(self):
+        print(f"[{self.likes} likes] {self.user.username}: {self.description}")
+
+
+class Comments:
+    def __init__(self, comment, user, post, likes=0):
+        self.comment = comment
+        self.likes = likes
+        self.user = user
+        self.post = post
+
+    def show_comment(self):
+        print(f"{self.user.username} commented: '{self.comment}' ({self.likes} likes)")
+
+
+class Message:
+    def __init__(self, text, sender, receiver):
+        self.text = text
+        self.sender = sender
+        self.receiver = receiver
+
+    def send(self):
+        print(f"Message from {self.sender.username} to {self.receiver.username}: {self.text}")
+
+
+# Instances
+user_lesly = User("Lesly", "Terrazo", "lesly_terrazor", "1234")
+user_carlos = User("Carlos", "Perez", "carlos_p", "abcd")
+
+post_lesly = Post("ex test", user_lesly, likes=3)
+user_lesly.add_post(post_lesly)
+
+comment = Comments("comment", user_carlos, post_lesly, likes=1)
+comment.show_comment()
+
+message = Message("msg", user_carlos, user_lesly)
+message.send()
+
+user_lesly.show_posts()
